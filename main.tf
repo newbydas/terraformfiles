60d19c232134383ec08c27af1d696bc77774fd25
newbydas <41777129+newbydas@users.noreply.github.com>
2021-04-23 17:34:47 +0530

    Updated main.tf with new values

--- main.tf
+++ main.tf
@@ -1,57 +1,57 @@
 variable "vpc_cidr_block" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "jhkjhbmnb"
+default     = "test_val1"
 }
 
 variable "pub_sub_cidr" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "hkh"
+default     = "test_val2"
 }
 
 variable "pvt_sub_cidr" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "j"
+default     = "test_val3"
 }
 
 variable "routetable1_cidr_block" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "kjhkhhkn"}
+default     = "test_val4"}
 
 variable "routetable2_cidr_block" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "bnmBBNB"}
+default     = "test_val5"}
 
 variable "ami" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "J"
+default     = "test_val6"
 }
 
 variable "region" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "H"
+default     = "test_val7"
 }
 
 variable "instance_type" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "JGHJG"
+default     = "test_val8"
 }
 
 variable "tagname" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "JG"
+default     = "test_val9"
 }
 
 variable "ebs_volume_size" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "JHJHK"
+default     = "test_val10"
 }
