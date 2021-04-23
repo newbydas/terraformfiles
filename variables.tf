af44bf1cda9ee2a83390b77d6f9cd4f007361baf
newbydas <41777129+newbydas@users.noreply.github.com>
2021-04-23 15:19:45 +0530

    Added all 10 variables of VPC and EC2

--- variables.tf
+++ variables.tf
@@ -1,1 +1,59 @@
+variable "vpc_cidr_block" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
 
+variable "pub_sub_cidr" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "pvt_sub_cidr" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "routetable1_cidr_block" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "routetable2_cidr_block" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "ami" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "instance_type" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "tagname" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "ebs_volume_size" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
