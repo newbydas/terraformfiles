e1b0567bd99a9b853bb2c235b560d08a27aa1223
newbydas <41777129+newbydas@users.noreply.github.com>
2021-04-23 17:08:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,59 @@
-{"vpc_cidr_block": "jhjh", "pub_sub_cidr": "jh", "pvt_sub_cidr": "jh", "routetable1_cidr_block": "jkh", "routetable2_cidr_block": "jkh", "ami": "jh", "region": "jh", "instance_type": "jkh", "tagname": "jh", "ebs_volume_size": "jkh"}
+variable "vpc_cidr_block" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "pub_sub_cidr" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "pvt_sub_cidr" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "routetable1_cidr_block" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "routetable2_cidr_block" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "ami" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "instance_type" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "tagname" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
+
+variable "ebs_volume_size" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = ""
+}
