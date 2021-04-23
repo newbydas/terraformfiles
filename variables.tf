6bff9a306bf8de5470dcc5f1c49baa7d6636c93d
newbydas <41777129+newbydas@users.noreply.github.com>
2021-04-23 16:53:03 +0530

    Updated variables.tf with new values

--- variables.tf
+++ variables.tf
@@ -1,1 +1,1 @@
-{"vpc_cidr_block": "kjkljkl", "pub_sub_cidr": "jklj", "pvt_sub_cidr": "klj", "routetable1_cidr_block": "klj", "routetable2_cidr_block": "klj", "ami": "klj", "region": "ljhjg", "instance_type": "hjghg", "tagname": "iuhjn", "ebs_volume_size": "khkg"}
+{"vpc_cidr_block": "khhkj", "pub_sub_cidr": "h", "pvt_sub_cidr": "kjh", "routetable1_cidr_block": "uou", "routetable2_cidr_block": "j", "ami": "jj", "region": "fhjhiouo", "instance_type": "jhb", "tagname": "hkj", "ebs_volume_size": "hg"}
