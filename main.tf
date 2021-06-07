390665732af32742a3952c8eec5eacc40223e019
newbydas <41777129+newbydas@users.noreply.github.com>
2021-06-07 13:09:06 +0530

    Updated main.tf with new values

--- main.tf
+++ main.tf
@@ -1,57 +1,57 @@
 variable "vpc_cidr_block" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"
+default     = "newtemp_val2"
 }
 
 variable "pub_sub_cidr" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"
+default     = "newtemp_val2"
 }
 
 variable "pvt_sub_cidr" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"
+default     = "newtemp_val2"
 }
 
 variable "routetable1_cidr_block" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"}
+default     = "newtemp_val2"}
 
 variable "routetable2_cidr_block" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"}
+default     = "newtemp_val2"}
 
 variable "ami" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"
+default     = "newtemp_val2"
 }
 
 variable "region" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"
+default     = "newtemp_val2"
 }
 
 variable "instance_type" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"
+default     = "newtemp_val2"
 }
 
 variable "tagname" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"
+default     = "newtemp_val2"
 }
 
 variable "ebs_volume_size" {
 description = "Name to be used on all resources as prefix"
 type        = string
-default     = "newtempval1"
+default     = "newtemp_val2"
 }
